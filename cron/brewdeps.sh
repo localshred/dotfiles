f336799767fa7761f2b3a962ff31fdf8a5350b34
BJ Neilsen <bj.neilsen@gmail.com>
2015-09-01 22:26:26 -0600

    Fix commit message on updating vim plugins and brew formulae snapshots

--- cron/brewdeps.sh
+++ cron/brewdeps.sh
@@ -14,6 +14,7 @@ brew list --versions | sort > $brew_formulae_file
 git diff-index --quiet HEAD --
 [ $? -eq 0 ] && echo "No changes to brew formulae" && exit
 
+count=$(cat ${brew_formulae_file} | wc -l)
 git add $brew_formulae_file
-git commit -m "Update brew formulae file (cron ${0})"
+git commit -m "Snapshot ${count} brew formulae (cron ${0})"
 git push origin master
--- cron/vimplugins.sh
+++ cron/vimplugins.sh
@@ -9,18 +9,21 @@ mkdir -p $mydotfiles/data
 rm -f $vim_plugins_file
 touch $vim_plugins_file
 
-plugins=$(ls -d ~/.vim/bundle/*/ | cut -f1 -d'/' | sort -f)
-for plugin_dir in $plugins; do
-  pushd $plugin_dir > /dev/null
-    origin=$(git remote -v | grep origin | head -1 | awk '{print $2}')
-    sha=$(git rev-parse HEAD)
-    echo "${plugin_dir} ${origin} ${sha}" >> $vim_plugins_file
-  popd > /dev/null
-done
+pushd ~/.vim/bundle > /dev/null
+  plugins=$(ls -d */ | cut -f1 -d'/' | sort -f)
+  for plugin_dir in $plugins; do
+    pushd $plugin_dir > /dev/null
+      origin=$(git remote -v | grep origin | head -1 | awk '{print $2}')
+      sha=$(git rev-parse HEAD)
+      echo "${plugin_dir} ${origin} ${sha}" >> $vim_plugins_file
+    popd > /dev/null
+  done
+popd > /dev/null
 
 git diff-index --quiet HEAD --
 [ $? -eq 0 ] && echo "No changes to vim plugins" && exit
 
+count=$(cat ${vim_plugins_file} | wc -l)
 git add $vim_plugins_file
-git commit -m "Update list of vim plugins in ~/.vim/bundle (cron ${0})"
+git commit -m "Snapshot ${count} vim plugins in ~/.vim/bundle (cron ${0})"
 git push origin master
