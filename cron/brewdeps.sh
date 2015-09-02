6137c54724f3587a92676b937f37cc9410ca1386
BJ Neilsen <bj.neilsen@gmail.com>
2015-09-01 21:45:27 -0600

    Add script to export a list of all brew dependencies on a weekly basis

---
+++ cron/brewdeps.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+if [[ -z "$mydotfiles" ]]; then
+  echo '$mydotfiles is empty'
+  exit 1
+fi
+
+cd $mydotfiles
+brew_formulae_file=$mydotfiles/data/brew-formulae.txt
+mkdir -p $mydotfiles/data
+touch $brew_formulae_file
+brew list --versions | sort > $brew_formulae_file
+
+git diff-index --quiet HEAD --
+[ $? -eq 0 ] && echo "No changes to brew formulae" && exit
+
+git add $brew_formulae_file
+git commit -m "Update brew formulae file (cron)"
+git push origin master
