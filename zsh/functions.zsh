2dea572a8985442aa695255d3ca5cfd65224038c
BJ Neilsen <bj.neilsen@gmail.com>
2016-09-15 15:34:23 -0600

    Remove unused zsh functions, alphabetize file

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -14,34 +14,30 @@ function dash()
   open "dash://$@"
 }
 
-function git_cur_branch()
-{
-  ref=$(git symbolic-ref HEAD 2> /dev/null) || return
-  "${ref#refs/heads/}"
-}
-
-# Grep ps output
-function psgrep()
-{
-  ps aux | grep $@
-}
-
-# Grep the history.
 function hgrep()
 {
   history | grep $@
 }
 
-# Cat the contents of the file and put it in
-# the pastboard.
-#
-#   `pbc foo.txt`
+function lintchanged() {
+  git diff --name-only | xargs ./node_modules/.bin/eslint --quiet
+}
+
 function pbc()
 {
   cat $@ | pbcopy
 }
 
-# Synchronize all local branches, reporting if any failed
+function psgrep()
+{
+  ps aux | grep $@
+}
+
+function recent() {
+  period=${1:-15m}
+  find . -type f -mtime -$period | grep -v '\.git'
+}
+
 function reposync()
 {
   color_red="\e[31m"
@@ -93,125 +89,6 @@ function reposync()
 function take() {
   mkdir -p $1
   cd $1
-}
-
-# Upstream out-of-date branch check (e.g. what does master have that qa doesn't)
-function upcherry() {
-  echo 'Upstream out-of-date branch check'
-
-  echo '[master > stage]'
-  git ch -v stage master
-
-  echo '[stage > prod]'
-  git ch -v prod stage
-}
-
-# Downstream out-of-date branch check (e.g. what does stable have that master doesn't)
-function dncherry() {
-  echo 'Downstream out-of-date branch check'
-
-  echo '[master < stage]'
-  git ch -v master stage
-
-  echo '[stage < prod]'
-  git ch -v stage prod
-}
-
-# Run the upcherry command against all repos (does not assume up-to-date)
-function srvupcherry()
-{
-  for repo in $(ls /code/src/services);
-  do
-    echo $(basename $repo)
-    cd $repo
-    upcherry
-    echo '----'
-    echo
-  done
-}
-
-# Run the dncherry command against all repos (does not assume up-to-date)
-function srvdncherry()
-{
-  for repo in $(ls /code/src/services);
-  do
-    echo $(basename $repo)
-    cd $repo
-    dncherry
-    echo '----'
-    echo
-  done
-}
-
-function srvreposync()
-{
-  for repo in $(ls /code/src/services);
-  do
-    echo $(basename $repo)
-    cd $repo
-    reposync
-    echo '----'
-  done
-}
-
-function sand()
-{
-  1=${1:=`basename $(pwd)`}
-  2=${2:="1"}
-  echo "ssh deployer@po-sand-$1$2"
-  ssh deployer@po-sand-$1$2
-}
-
-function qa()
-{
-  1=${1:=`basename $(pwd)`}
-  2=${2:="1"}
-  echo "ssh deployer@po-qa-$1$2"
-  ssh deployer@po-qa-$1$2
-}
-
-function int()
-{
-  1=${1:=`basename $(pwd)`}
-  2=${2:="sa"}
-  3=${3:="1"}
-  echo "ssh bj@${2}-int-$1$3"
-  ssh bj@${2}-int-$1$3
-}
-
-function stage()
-{
-  1=${1:=`basename $(pwd)`}
-  3=${2:="1"}
-  echo "ssh bj@sc-stage-$1$2"
-  ssh bj@sc-stage-$1$2
-}
-
-function prod()
-{
-  1=${1:=`basename $(pwd)`}
-  2=${2:="sb"}
-  3=${3:="1"}
-  echo "ssh bj@${2}-prod-$1$3"
-  ssh bj@${2}-prod-$1$3
-}
-
-function zsh_stats()
-{
-  history | awk '{CMD[$2]++;count++;}END { for (a in CMD)print CMD[a] " " CMD[a]/count*100 "% " a;}' | grep -v "./" | column -c3 -s " " -t | sort -nr | nl |  head -n20
-}
-
-function ptreb() {
-  bin/treb "$@" > log/deploy.log | egrep '(=+\[\w+\]===|(Currently|^\s+\*) executing|^Group \d+|^cap |^ \*\* )'
-}
-
-function lintchanged() {
-  git diff --name-only | xargs ./node_modules/.bin/eslint --quiet
-}
-
-function recent() {
-  period=${1:-15m}
-  find . -type f -mtime -$period | grep -v '\.git'
 }
 
 function vimupdateplugins() {
