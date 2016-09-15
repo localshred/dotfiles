59f158928ee463e6cc5c3c057b1033f98b2e8b81
BJ Neilsen <bj.neilsen@gmail.com>
2016-09-15 10:41:28 -0600

    Add vimupdateplugins to update vim plugins via git with a nice stdout report

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -203,3 +203,22 @@ function recent() {
   period=${1:-15m}
   find . -type f -mtime -$period | grep -v '\.git'
 }
+
+function vimupdateplugins() {
+  for plugin_dir in $(find ~/.vim/bundle -type d -maxdepth 1 -mindepth 1); do
+    pushd $plugin_dir > /dev/null
+    git fetch origin master --quiet
+    local_head=$(git rev-parse --short HEAD)
+    remote_head=$(git rev-parse --short origin/master)
+    plugin_name=$(basename $plugin_dir)
+    if [[ $local_head != $remote_head ]]
+    then
+      commits_behind=$(git cherry $local_head $remote_head | wc -l | awk '{print $1 " commits behind"}')
+      echo "Updating ${plugin_name} from ${local_head} to ${remote_head} ($commits_behind)"
+      git rebase origin/master --quiet
+    else
+      echo "${plugin_name} is already up to date"
+    fi
+    popd > /dev/null
+  done
+}
