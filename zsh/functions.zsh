b941c6612c923541cd6ea6eb2c7a0c16a6b64077
BJ Neilsen <bj.neilsen@gmail.com>
2016-10-12 10:55:23 -0600

    Add npmver and npmedit commands for introspecting/editing node_modules

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -23,6 +23,16 @@ function lintchanged() {
   git diff --name-only | xargs ./node_modules/.bin/eslint --quiet
 }
 
+function npmedit() {
+  package=$1
+  vim node_modules/$package/package.json -c "NERDTree node_modules/$package"
+}
+
+function npmver() {
+  package=$1
+  jq '.version' node_modules/$package/package.json | awk -F\" '{print $2}'
+}
+
 function pbc()
 {
   cat $@ | pbcopy
