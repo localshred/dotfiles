0d7ddc410a604f6089b2c3465875b5ce3d48142f
Kelvin Jasperson <github@mqp3.com>
2017-01-17 10:55:09 -0700

    fix color variable names

--- zsh/git.zsh
+++ zsh/git.zsh
@@ -12,7 +12,7 @@ function reposync()
   git status &> /dev/null
   if [ $? -eq 0 ]; then
     git diff-index --quiet HEAD --
-    [ $? -ne 0 ] && echo "${color_yellow}You have local changes that haven't been commited${reset_color}" && return
+    [ $? -ne 0 ] && echo "${color_yellow}You have local changes that haven't been commited${color_reset}" && return
     git remote update origin --prune > /dev/null
 
     missing=()
@@ -42,11 +42,10 @@ function reposync()
 
     current_branch=$(git rev-parse --abbrev-ref HEAD)
     [ $start_branch != $current_branch ] && git checkout $start_branch --quiet
-    [[ -n "${up_to_date}" ]] && for branch in $up_to_date; do echo "${color_green}[${branch}] up to date${reset_color}"; done
-    [[ -n "${updated}" ]] && for updated_description in $updated; do echo "${color_green}${updated_description}${reset_color}"; done
-    [[ -n "${missing}" ]] && for branch in $missing; do echo "${color_yellow}[${branch}] missing on origin${reset_color}"; done
+    [[ -n "${up_to_date}" ]] && for branch in $up_to_date; do echo "${color_green}[${branch}] up to date${color_reset}"; done
+    [[ -n "${updated}" ]] && for updated_description in $updated; do echo "${color_green}${updated_description}${color_reset}"; done
+    [[ -n "${missing}" ]] && for branch in $missing; do echo "${color_yellow}[${branch}] missing on origin${color_reset}"; done
   else
-    echo "${red_color}Not a git repository${reset_color}"
+    echo "${color_red}Not a git repository${color_reset}"
   fi
 }
-
