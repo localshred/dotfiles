e9b3f2b6ee3077ead732cf942fcae5c0fe0b2ac0
BJ Neilsen <bj.neilsen@gmail.com>
2019-04-11 12:53:58 -0400

    Change the RPROMPT to indicate language:version
    Based on known files/setup in project directory:
    
    + node -> node_modules directory present
    + elixir -> mix.exs file present
    + go -> at least one .go file in top two directories
    + ruby -> Gemfile file present

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,8 +1,34 @@
+function get_language_prompt() {
+  lang=""
+  version=""
+  if [[ -d node_modules ]]; then
+    lang="js"
+    version=$(nvm current)
+  elif [[ -f mix.exs ]]; then
+    lang="elixir"
+    if [[ -f .kiexrc ]]; then
+      version=$(cat .kiexrc)
+    fi
+    if [[ -z "$version" ]]; then
+      version=$(kiex list | grep "=\* elixir" | awk -F- '{print $2}')
+    fi
+    kiex use $version > /dev/null
+  elif [[ ! -z $(find . -name '*.go' -type f -maxdepth 2) ]]; then
+    lang="go"
+    version=$(go version | awk -Fgo '{print $3}' | awk '{print $1}')
+  elif [[ -s Gemfile ]]; then
+    lang="ruby"
+    version=$(rbenv version | awk '{print $1}')
+  fi
+  if [[ ! -z $lang && ! -z $version ]]; then
+    echo " %{$fg[red]%}${lang}:${version}%{$reset_color%}"
+  fi
+}
+
 PROMPT='$(git_prompt_info)%(?,,%{${fg_bold[white]}%}[%?]%{$reset_color%} )%{$fg[magenta]%}%#%{$reset_color%} '
-RPROMPT='%{$fg[green]%}%~%{$reset_color%} %{$fg[red]%}$(nvm current)%{$reset_color%}'
+RPROMPT='%{$fg[green]%}%~%{$reset_color%}$(get_language_prompt)'
 
 ZSH_THEME_GIT_PROMPT_PREFIX="%{$fg[blue]%}["
 ZSH_THEME_GIT_PROMPT_SUFFIX="%{$reset_color%} "
 ZSH_THEME_GIT_PROMPT_DIRTY="%{$fg[blue]%}]%{$fg[red]%}💥 %{$reset_color%}"
 ZSH_THEME_GIT_PROMPT_CLEAN="%{$fg[blue]%}]"
-
