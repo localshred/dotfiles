42b2198376fd94e89ca6e4547d8fd784fdf7bbb4
BJ Neilsen <bj.neilsen@gmail.com>
2019-05-09 11:50:52 -0400

    Add task to list of items to nocorrect

--- zsh/lib/nocorrect.zsh
+++ zsh/lib/nocorrect.zsh
@@ -1,6 +1,6 @@
 #!/usr/bin/env zsh
 
-local programs=(man mv mysql mkdir erl curl rake make cake lessc lunchy sb)
+local programs=(cake curl erl lessc lunchy make man mkdir mv mysql rake sb task)
 
 __load_correction() {
     setopt correct_all
@@ -8,17 +8,5 @@ __load_correction() {
       eval "alias $program='nocorrect $program'"
     done
     SPROMPT="zsh: correct $fg[red]%R$reset_color to $fg[green]%r$reset_color [nyae]? "
-    # alias man='nocorrect man'
-    # alias mv='nocorrect mv'
-    # alias mysql='nocorrect mysql'
-    # alias mkdir='nocorrect mkdir'
-    # alias erl='nocorrect erl'
-    # alias curl='nocorrect curl'
-    # alias rake='nocorrect rake'
-    # alias make='nocorrect make'
-    # alias cake='nocorrect cake'
-    # alias lessc='nocorrect lessc'
-    # alias lunchy='nocorrect lunchy'
-    # alias sb='nocorrect sb'
 }
 
