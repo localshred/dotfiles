b9eead1d833ef937e20e2159a6df0d5b03101a6f
BJ Neilsen <bj.neilsen@gmail.com>
2019-12-06 15:37:04 -0500

    Deindent reposync

--- zsh/lib/git.zsh
+++ zsh/lib/git.zsh
@@ -12,44 +12,45 @@ function reposync()
   color_reset="\e[39m"
 
   git status &> /dev/null
-  if [ $? -eq 0 ]; then
-    git diff-index --quiet HEAD --
-    [ $? -ne 0 ] && echo "${color_yellow}You have local changes that haven't been commited${color_reset}" && return
-    git remote update origin --prune > /dev/null
+  [ $? -ne 0 ] && echo "${color_red}Not a git repository${color_reset}" && return
 
-    missing=()
-    up_to_date=()
-    updated=()
-    start_branch=$(git rev-parse --abbrev-ref HEAD)
-    for branch in $(git for-each-ref --format='%(refname:short)' refs/heads/)
-    do
-      git rev-parse origin/$branch &> /dev/null
-      if [[ $? -eq 0 ]]
+  git diff-index --quiet HEAD --
+  [ $? -ne 0 ] && echo "${color_yellow}You have local changes that haven't been commited${color_reset}" && return
+
+  git remote update origin --prune > /dev/null
+  [ $? -ne 0 ] && echo "${color_red}Could not fetch origin${color_reset}" && return
+
+  missing=()
+  up_to_date=()
+  updated=()
+  start_branch=$(git rev-parse --abbrev-ref HEAD)
+  for branch in $(git for-each-ref --format='%(refname:short)' refs/heads/)
+  do
+    git rev-parse origin/$branch &> /dev/null
+    if [[ $? -eq 0 ]]
+    then
+      local_head=$(git rev-parse --short $branch)
+      remote_head=$(git rev-parse --short origin/$branch)
+      if [[ $local_head != $remote_head ]]
       then
-        local_head=$(git rev-parse --short $branch)
-        remote_head=$(git rev-parse --short origin/$branch)
-        if [[ $local_head != $remote_head ]]
-        then
-          git checkout $branch --quiet
-          commits_behind=$(git rev-list $remote_head "^$local_head" --count)
-          updated+="[${branch}] rebase from ${local_head} to ${remote_head} ($commits_behind commits behind)"
-          git rebase origin/$branch --quiet
-        else
-          up_to_date+=$branch
-        fi
+        git checkout $branch --quiet
+        commits_behind=$(git rev-list $remote_head "^$local_head" --count)
+        updated+="[${branch}] rebase from ${local_head} to ${remote_head} ($commits_behind commits behind)"
+        git rebase origin/$branch --quiet
       else
-        missing+=$branch
+        up_to_date+=$branch
       fi
-    done
+    else
+      missing+=$branch
+    fi
+  done
 
-    current_branch=$(git rev-parse --abbrev-ref HEAD)
-    [ $start_branch != $current_branch ] && git checkout $start_branch --quiet
-    [[ -n "${up_to_date}" ]] && for branch in $up_to_date; do echo "${color_green}[${branch}] up to date${color_reset}"; done
-    [[ -n "${updated}" ]] && for updated_description in $updated; do echo "${color_green}${updated_description}${color_reset}"; done
-    [[ -n "${missing}" ]] && for branch in $missing; do echo "${color_yellow}[${branch}] missing on origin${color_reset}"; done
-  else
-    echo "${color_red}Not a git repository${color_reset}"
-  fi
+  current_branch=$(git rev-parse --abbrev-ref HEAD)
+  [ $start_branch != $current_branch ] && git checkout $start_branch --quiet
+
+  [[ -n "${up_to_date}" ]] && for branch in $up_to_date; do echo "${color_green}[${branch}] up to date${color_reset}"; done
+  [[ -n "${updated}" ]] && for updated_description in $updated; do echo "${color_green}${updated_description}${color_reset}"; done
+  [[ -n "${missing}" ]] && for branch in $missing; do echo "${color_yellow}[${branch}] missing on origin${color_reset}"; done
 }
 
 function reposyncdirs()
