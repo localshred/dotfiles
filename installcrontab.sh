19396756938cec54722195eea3646d834bad217f
BJ Neilsen <bj.neilsen@gmail.com>
2020-01-17 10:27:06 -0500

    Pull crontab installation into install.sh file

--- installcrontab.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-crontabfile=$DOTFILES/crontab.cron
-who=`whoami`
-
-echo "Installing crontab ${crontabfile}..."
-echo "sudo crontab -u ${who} $crontabfile"
-sudo crontab -u $who $crontabfile
-crontab -l
