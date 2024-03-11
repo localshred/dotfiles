aef561927a60f5a2b457d2452edfda3865eff41f
BJ Neilsen <bj.neilsen@gmail.com>
2024-03-11 15:51:31 -0400

    Elixir + M1 things

--- zsh/lib/elixir.zsh
+++ zsh/lib/elixir.zsh
@@ -1,5 +1,7 @@
 #!/usr/bin/env zsh
 
+export ERL_AFLAGS="-kernel shell_history enabled"
+
 function elixir_prompt() {
   if [[ -s mix.exs ]]; then
     version=$(asdf current elixir | awk '{print $2}')
